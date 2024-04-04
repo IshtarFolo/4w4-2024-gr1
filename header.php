7c1b218b36a06ea4f8c915b6e97138a061fb1bb3
Arbour <e1071853@cmaisonneuve.qc.ca>
2024-04-04 15:16:06 -0400

    tp1 modifications au menu dans le footer

--- header.php
+++ header.php
@@ -23,6 +23,6 @@
             <?php get_template_part("gabarits/btn-burger") ?> 
             <!-- Fin btn-burger -->
             <header class="menu__header">
-                <?php wp_nav_menu(array("container" => "nav")); ?>
+            <?php wp_nav_menu(array("theme_location" => "principal")); ?>
             </header>
         </div>

1fc104ddad578fde4ffe80cc1f722d1a10fc382a
Xavier Arbour <119866415+IshtarFolo@users.noreply.github.com>
2024-04-10 10:23:29 -0400

    tp2 modifications dans front-page.php et footer.php

--- front-page.php
+++ front-page.php
@@ -26,11 +26,11 @@
             if (have_posts()) :
                 while (have_posts()) : the_post(); 
             ?>
-                    <div class="carte bck-primaire-100">
-                        <?php the_post_thumbnail("thumbnail"); ?> 
+                    <div class="carte bck-primaire-100"> 
                         <h3><?php the_title(); ?></h3>
                         <p><?php echo wp_trim_words(get_the_content(), 10); ?></p>
                         <?php the_category(); ?>
+						<?php the_post_thumbnail('full'); ?>
                         <!-- Ajoute un lien vers l'article selctionné -->
                         <a href="<?php the_permalink(); ?>">Suite</a>
                     </div>

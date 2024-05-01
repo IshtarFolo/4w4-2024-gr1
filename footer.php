733af7496064e23c3369cd7b500e6ab051fc5304
Xavier Arbour <119866415+IshtarFolo@users.noreply.github.com>
2024-05-01 09:44:22 -0400

    s12c1 ajout d'icones dans le footer et style du footer update

--- footer.php
+++ footer.php
@@ -7,9 +7,12 @@
             <img class="logo-footer" src="http://localhost:8085/4w4-2024-gr1/wp-content/uploads/2024/04/voyage4.png" alt="">
             <h2>Nous Joindre</h2>
         </div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, aperiam dolorem est quam repellendus tenetur sit ipsum natus voluptas fugit, dignissimos dolorum corrupti pariatur dolor! Repellendus accusantium voluptatem eos rerum.
-            Cum perspiciatis expedita adipisci voluptates harum vitae, corporis placeat soluta aliquid in velit nam animi repellat molestiae. Laboriosam sit fugiat doloremque fuga expedita enim aperiam dolorem, explicabo error odio iure!
-            Distinctio odit quibusdam vel voluptatem quasi architecto placeat veniam facilis vitae, neque expedita sequi natus omnis alias ipsa labore deserunt. Suscipit qui tenetur a tempora itaque incidunt ea nesciunt velit?</p>
+        <div class="sociaux">
+            <img class="logo-sociaux" src="http://localhost:8085/4w4-2024-gr1/wp-content/uploads/2024/05/instagram.png" alt="">
+            <img class="logo-sociaux" src="http://localhost:8085/4w4-2024-gr1/wp-content/uploads/2024/05/twitter.png" alt="">
+            <img class="logo-sociaux" src="http://localhost:8085/4w4-2024-gr1/wp-content/uploads/2024/05/facebook.png" alt="">
+            <img class="logo-sociaux" src="http://localhost:8085/4w4-2024-gr1/wp-content/uploads/2024/05/linkedin.png" alt="">
+        </div>
             <?php wp_nav_menu(array("theme_location" => "bas")); ?>
             <a href="#" class="bouton-retour bck-primaire-300 clr-primaire-100">&#8593</a>
 
